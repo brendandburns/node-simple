880c3b432c847b7df5ff3e25e56b62d2b938c676
Brendan Burns <brendan.d.burns@gmail.com>
2016-10-19 21:06:24 -0700

    Add node-simple, update start.js

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Goodbye cruel world!\n");
+  response.end("Hello world!\n");
   console.log("Request received");
 });
 
