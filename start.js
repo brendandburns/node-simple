fefe869df30d82ca469e40c3a2c251497bc70f6e
Brendan Burns <brendan.d.burns@gmail.com>
2017-01-25 21:55:44 -0800

    markup2

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/html"});
   response.end("<h1>Hello, goodby world!\n</h1>");
   console.log("Request received");
 });
